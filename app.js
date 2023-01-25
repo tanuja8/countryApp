775bb5f8d1d471ccb35f02484a85667bc028e397
Tanuja <tanujakhatun292@gmail.com>
2023-01-25 23:21:52 +0530

    some things added

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const searchBtn= document.querySelector('.search-btn');
 const inputBar= document.querySelector('.input-bar');
+const result=document.querySelector('.result')
+
+searchBtn.addEventListener('click',()=>{
+fetch(`https://restcountries.com/v3.1/name/${inputBar.value}`)
+.then(res => res.json())
+.then((data) => displayCountries(data))
+});
+const displayCountries =(data ) =>{
+    console.log(data)
+    data.forEach((country) =>{
+        const div=document.createElement("div");
+        div.className="card";
+        div.innerHTML=`<img src="${country.flags.png}" class="card-img-top" alt="...">
+        <div class="card-body">
+         <h2 class ="card.text">${country.name.common}</h2>
+         </div>
+        `;
+        result.appendChild(div)
+    });
+};
